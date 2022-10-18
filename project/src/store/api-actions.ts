konovalov-as/guess-melody-10

7.23. Добавит компонент `react-toastify`.

project/src/store/api-actions.ts
@@ -2,22 +2,11 @@ import {AxiosInstance} from 'axios';
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import {AppDispatch, State} from '../types/state.js';
 import {Questions} from '../types/question';
-import {loadQuestions, requireAuthorization, setDataLoadedStatus, setError, redirectToRoute} from './action';
+import {loadQuestions, requireAuthorization, setDataLoadedStatus, redirectToRoute} from './action';
 import {saveToken, dropToken} from '../services/token';
-import {APIRoute, AuthorizationStatus, AppRoute, TIMEOUT_SHOW_ERROR} from '../const';
+import {APIRoute, AuthorizationStatus, AppRoute} from '../const';
 import {AuthData} from '../types/auth-data';
 import {UserData} from '../types/user-data';
-import {store} from './';
-
-export const clearErrorAction = createAsyncThunk(
-  'game/clearError',
-  () => {
-    setTimeout(
-      () => store.dispatch(setError(null)),
-      TIMEOUT_SHOW_ERROR,
-    );
-  },
-);
 
 export const fetchQuestionAction = createAsyncThunk<void, undefined, {
   dispatch: AppDispatch,

project/src/store/reducer.ts
@@ -1,5 +1,5 @@
 import {createReducer} from '@reduxjs/toolkit';
-import {incrementStep, checkUserAnswer, loadQuestions, setDataLoadedStatus, requireAuthorization, resetGame, setError} from './action';
+import {incrementStep, checkUserAnswer, loadQuestions, setDataLoadedStatus, requireAuthorization, resetGame} from './action';
 import {isAnswerCorrect} from '../game';
 import {FIRST_GAME_STEP, AuthorizationStatus} from '../const';
 import {Questions} from '../types/question';
@@ -12,7 +12,6 @@ type InitalState = {
   questions: Questions,
   authorizationStatus: AuthorizationStatus,
   isDataLoaded: boolean,
-  error: string | null,
 }
 
 const initialState: InitalState = {
@@ -21,7 +20,6 @@ const initialState: InitalState = {
   questions: [],
   authorizationStatus: AuthorizationStatus.Unknown,
   isDataLoaded: false,
-  error: null,
 };
 
 const reducer = createReducer(initialState, (builder) => {
@@ -46,9 +44,6 @@ const reducer = createReducer(initialState, (builder) => {
     })
     .addCase(requireAuthorization, (state, action) => {
       state.authorizationStatus = action.payload;
-    })
-    .addCase(setError, (state, action) => {
-      state.error = action.payload;
     });
 });
 

bugaboo-/guess-melody-11

project/src/store/api-actions.ts
@@ -2,22 +2,11 @@ import {AxiosInstance} from 'axios';
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import {AppDispatch, State} from '../types/state.js';
 import {Questions} from '../types/question';
-import {loadQuestions, requireAuthorization, setQuestionsDataLoadingStatus, setError, redirectToRoute} from './action';
+import {loadQuestions, requireAuthorization, setQuestionsDataLoadingStatus, redirectToRoute} from './action';
 import {saveToken, dropToken} from '../services/token';
-import {APIRoute, AuthorizationStatus, AppRoute, TIMEOUT_SHOW_ERROR} from '../const';
+import {APIRoute, AuthorizationStatus, AppRoute} from '../const';
 import {AuthData} from '../types/auth-data';
 import {UserData} from '../types/user-data';
-import {store} from './';
-
-export const clearErrorAction = createAsyncThunk(
-  'game/clearError',
-  () => {
-    setTimeout(
-      () => store.dispatch(setError(null)),
-      TIMEOUT_SHOW_ERROR,
-    );
-  },
-);
 
 export const fetchQuestionAction = createAsyncThunk<void, undefined, {
   dispatch: AppDispatch;

project/src/store/reducer.ts
@@ -1,5 +1,5 @@
 import {createReducer} from '@reduxjs/toolkit';
-import {incrementStep, checkUserAnswer, loadQuestions, setQuestionsDataLoadingStatus, requireAuthorization, resetGame, setError} from './action';
+import {incrementStep, checkUserAnswer, loadQuestions, setQuestionsDataLoadingStatus, requireAuthorization, resetGame} from './action';
 import {isAnswerCorrect} from '../game';
 import {FIRST_GAME_STEP, AuthorizationStatus} from '../const';
 import {Questions} from '../types/question';
@@ -12,7 +12,6 @@ type InitalState = {
   questions: Questions;
   authorizationStatus: AuthorizationStatus;
   isQuestionsDataLoading: boolean;
-  error: string | null;
 }
 
 const initialState: InitalState = {
@@ -21,7 +20,6 @@ const initialState: InitalState = {
   questions: [],
   authorizationStatus: AuthorizationStatus.Unknown,
   isQuestionsDataLoading: false,
-  error: null,
 };
 
 const reducer = createReducer(initialState, (builder) => {
@@ -46,9 +44,6 @@ const reducer = createReducer(initialState, (builder) => {
     })
     .addCase(requireAuthorization, (state, action) => {
       state.authorizationStatus = action.payload;
-    })
-    .addCase(setError, (state, action) => {
-      state.error = action.payload;
     });
 });
 